c75078280912095f59273f483914861bb480ea7a
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2024-03-10 14:22:35 +0100

    feat: add zad_1.py

---
+++ zad_1.py
@@ -0,0 +1,25 @@
+import utils as u
+
+# Zadanie a
+
+print('\nZadanie a')
+u.print_names(['Ania', 'Bartek', 'Szymon', 'Adam', 'Staszek'])
+
+# Zadanie b
+
+print('\nZadanie b1')
+print(u.multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
+
+print('\nZadanie b2')
+print(u.multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
+
+# Zadanie c
+
+
+print('\nZadanie c')
+u.even_numbers(range(0, 10))
+
+# Zadanie d
+
+print('\nZadanie d')
+u.every_second_number(range(0, 10))
