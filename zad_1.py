0b2d63da639f2d7c00e60662a3c9bcfb34d5b7dc
Karol Stawowski <stawowski.dev@gmail.com>
2024-03-20 20:30:18 +0100

    chore: add wrong code

--- zad_1.py
+++ zad_1.py
@@ -3,8 +3,13 @@ class Student:
         self.name = name
         self.marks = marks
 
+        
+
     def is_passed(self) -> bool:
         if (sum(self.marks) / len(self.marks)) > 50:
+
+
+
             return True
         return False
 
@@ -12,5 +17,5 @@ class Student:
 s1 = Student("Adam", [62, 80, 25])
 print(s1.is_passed())
 
-s2 = Student("Jola", [15, 57, 43])
+s2 = Student("Jola", [15, 57, 443])
 print(s2.is_passed())
